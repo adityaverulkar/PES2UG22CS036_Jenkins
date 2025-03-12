0ac2f3964347c58d4acbfc6a04fd6780c1c51d45
adityaverulkar <aditver12@gmail.com>
2025-03-12 13:27:44 +0530

    Aditya Verulkar - PES2UG22CS036

---
+++ main/hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I am Aditya Verulkar\n";
+  cout << "My SRN is PES2UG22CS036\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
